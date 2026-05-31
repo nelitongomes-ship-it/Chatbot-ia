1613cdb4f0ad37db75ffc372f4527e7f1e6e4a82
nelitongomes-ship-it <253819507+nelitongomes-ship-it@users.noreply.github.com>
2026-05-31 18:02:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -759,6 +759,55 @@ Exemplo:
 
   return res.sendStatus(200);
 }
+// =====================================================
+// LISTAR CLIENTES
+// =====================================================
+
+if (
+  message === "/listarclientes" &&
+  adminSessions[phone]
+) {
+
+  const clientes =
+    await prisma.client.findMany({
+      orderBy: {
+        createdAt: "desc"
+      }
+    });
+
+  if (!clientes.length) {
+
+    await sendMessage(
+      phone,
+      "📭 Nenhum cliente cadastrado."
+    );
+
+    return res.sendStatus(200);
+  }
+
+  let resposta =
+    "📋 CLIENTES CADASTRADOS\n\n";
+
+  clientes.forEach((cliente, index) => {
+
+    resposta +=
+`${index + 1}️⃣ ${cliente.name}
+
+📱 ${cliente.phone}
+📦 ${cliente.planType}
+🟢 ${cliente.isActive ? "ATIVO" : "INATIVO"}
+
+`;
+  });
+
+  await sendMessage(
+    phone,
+    resposta
+  );
+
+  return res.sendStatus(200);
+}
+    
     // =====================================================
 // ALTERAR SENHA
 // =====================================================
