e6f165d2dea0fde4188ce38869f4ee566bd3f140
nelitongomes-ship-it <253819507+nelitongomes-ship-it@users.noreply.github.com>
2026-06-01 06:47:17 -0300

    Implement cron job for appointment reminders
    Added a cron job to check for appointments and send reminders.

--- lembretes.js
+++ lembretes.js
@@ -1,1 +1,64 @@
 
+const cron = require("node-cron");
+
+module.exports = function (prisma, sendMessage) {
+
+  cron.schedule("* * * * *", async () => {
+
+    try {
+
+      console.log("Verificando agenda...");
+
+      const agora = new Date();
+
+      const dia =
+        String(agora.getDate()).padStart(2, "0");
+
+      const mes =
+        String(agora.getMonth() + 1).padStart(2, "0");
+
+      const ano =
+        agora.getFullYear();
+
+      const dataHoje =
+        `${dia}/${mes}/${ano}`;
+
+      const horaAtual =
+        `${String(agora.getHours()).padStart(2, "0")}:${String(agora.getMinutes()).padStart(2, "0")}`;
+
+      const compromissos =
+        await prisma.appointment.findMany();
+
+      for (const item of compromissos) {
+
+        if (
+          item.date === dataHoje &&
+          item.time === horaAtual
+        ) {
+
+          await sendMessage(
+            item.phone,
+`⏰ LEMBRETE
+
+Você possui um compromisso agora.
+
+🕒 ${item.time}
+📝 ${item.description}`
+          );
+
+        }
+
+      }
+
+    } catch (erro) {
+
+      console.log(
+        "ERRO LEMBRETES:",
+        erro
+      );
+
+    }
+
+  });
+
+};
