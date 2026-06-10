da81edd4a827cbe923c8308745255cffe1a9b883
nelitongomes-ship-it <253819507+nelitongomes-ship-it@users.noreply.github.com>
2026-06-10 15:07:01 -0300

    Add buscarDolar function to fetch USD-BRL rates

--- cod.pesquisa/dolar.js
+++ cod.pesquisa/dolar.js
@@ -1,1 +1,21 @@
 
+const axios = require("axios");
+
+async function buscarDolar() {
+
+  const response =
+    await axios.get(
+      "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+    );
+
+  return {
+    compra:
+      response.data.USDBRL.bid,
+    venda:
+      response.data.USDBRL.ask
+  };
+}
+
+module.exports = {
+  buscarDolar
+};
