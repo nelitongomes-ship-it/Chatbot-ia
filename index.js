5abd8f67d8725832c1646c7c74b9d0ee02098dd6
nelitongomes-ship-it <253819507+nelitongomes-ship-it@users.noreply.github.com>
2026-06-12 20:58:09 -0300

    Comment out lembretes import in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const iniciarLembretes = require("./lembretes");
+//const iniciarLembretes = require("./lembretes");
 const recuperarSenha = require("./comandos/admin");
 const bloqueios = require("./comandos/bloqueios");
 const estatisticas = require("./comandos/estatisticas");
